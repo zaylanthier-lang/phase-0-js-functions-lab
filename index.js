c3d7909ee39589bf6a83184dc1f5589810e0a8df
Zayden <zaylanthier@gmail.com>
2026-03-04 13:04:34 -0500

    Index.js Coded

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-
-
-
-
+function calculateTax(amount){
+    return amount * 0.10;
+}
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+function findMaximum(num1,num2){
+    if(num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
+function isPalindrome(word){
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
+function calculateDiscountedPrice(originalPrice,discountPercentage){
+    const discountAmount = originalPrice * (discountPercentage /100);
+    return originalPrice - discountAmount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
